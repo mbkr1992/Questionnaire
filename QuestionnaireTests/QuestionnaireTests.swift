95b88abb2180a680784a4f096d315dd5f1ea14cb
Abubakr Dar <mbkr1992@gmail.com>
2018-05-06 22:12:18 +0200

    -- setup CI

--- QuestionnaireTests/QuestionnaireTests.swift
+++ QuestionnaireTests/QuestionnaireTests.swift
@@ -8,31 +8,20 @@
 
 import XCTest
 import Quick
+import Nimble
 
 @testable import Questionnaire
 
 
-class QuestionnaireTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        XCTAssert(true)
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class QuestionnaireTests: QuickSpec {
+    override func spec() {
+        describe("test describle") {
+            beforeEach {
+            }
+            
+            it("should work", closure: {
+                expect(123).to(equal(123))
+            })
         }
     }
-    
 }
